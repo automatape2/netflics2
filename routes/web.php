faf65cc17d039ee6efe1f8f0ef015ad4f522f57b
Jose Junior Anicama Tanta <j.j.anicamatanta@gmail.com>
2018-11-06 10:14:00 -0500

    update

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return redirect()->route('hola');
+});
+
+Route::get('/test/hola', function () {
+    return "Hola";
+})->name('hola');
